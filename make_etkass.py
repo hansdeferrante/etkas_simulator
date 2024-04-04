fe2b6f544aac36f8c814e0cb1f15defa9e128ac0
H.C. de Ferrante <hansdeferrante@outlook.com>
2024-04-04 13:47:44 +0200

    Doing ETKAs simulations

---
+++ make_etkass.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  2 17:33:44 2022
+
+@author: H.C. de Ferrante
+"""
+
+import sys
+import os
+import pandas as pd
+
+from simulator.code.ETKASS import ETKASS
+from simulator.code.read_input_files import read_sim_settings
+import simulator.magic_values.etkass_settings as es
+import simulator.code.read_input_files as rdr
+import simulator.magic_values.column_names as cn
+from datetime import datetime
+
+if __name__ == '__main__':
+    pd.set_option('display.max_rows', 500)
+
+    sim_set = read_sim_settings(
+                os.path.join(
+                    es.DIR_SIM_SETTINGS,
+                    '2024-03-19',
+                    'CurrentETKAS_validation_1_1.yml'
+                )
+            )
+
+    # Read in simulation settings
+    simulator = ETKASS(
+        sim_set=sim_set,
+        verbose=0
+    )
+
+    ml = simulator.simulate_allocation(
+        verbose=False
+    )
+
+    simulator.sim_results.save_outcomes_to_file(
+        patients=simulator.patients,
+        cens_date=sim_set.SIM_END_DATE
+    )
